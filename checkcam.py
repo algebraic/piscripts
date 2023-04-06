cef644a6334b986c587f0624cf5a1fdd819fd2de
Zach <zach.johnson47@gmail.com>
2023-04-06 14:21:06 -0400

    cleanup

--- checkcam.py
+++ checkcam.py
@@ -7,11 +7,6 @@ log = logging.getLogger()
 log.setLevel(logging.INFO)
 logformat = logging.Formatter("%(levelname)s %(asctime)s (%(name)s) %(message)s")
 
-# stdout handler
-# ch = logging.StreamHandler(sys.stdout)
-# ch.setFormatter(logformat)
-# log.addHandler(ch)
-# file handler
 fh = handlers.RotatingFileHandler("logs/wyzecam.log", maxBytes=(1048576*5), backupCount=7)
 fh.setFormatter(logformat)
 log.addHandler(fh)
@@ -32,34 +27,8 @@ for attempt in range(1, max_attempts + 1):
     else:
         log.debug("Ping attempt {attempt} failed. Retrying...")
 
-        # If this is the last attempt, write a log message
+        # Perform action if this is the last attempt
         if attempt == max_attempts:
             msg = ["Camera Offline", "driveway cam seems to be offline", ""]
             simplepush.ppushitrealgood(msg, True)
             log.warning("Failed to ping {ip_address} after {max_attempts} attempts.")
-
-
-
-
-def ping(host):
-    log.debug("ping function: " + str(host))
-
-    param = '-n' if platform.system().lower()=='windows' else '-c'
-    command = ['ping', param, '1', host]
-    result = subprocess.run(command, stdout=subprocess.PIPE)
-    
-    log.debug("result: " + str(result))
-    
-    output = result.stdout.decode('utf8')
-    if "Request timed out." in output or "100% packet loss" in output:
-        msg = ["Camera Offline", "driveway cam seems to be offline", ""]
-        simplepush.ppushitrealgood(msg, True)
-        return "OFFLINE"
-    
-    # msg = ["Camera Online", "yay, cam " + str(host) + " is online", ""]
-    # simplepush.ppushitrealgood(msg, True)
-    return "online"
-
-hostname = '192.168.86.57'
-output = ping(hostname)
-log.info("driveway cam " + str(output))
