11a4714135135c88f2959ea29f37be6aa0d27277
Zach <zach.johnson47@gmail.com>
2023-04-06 12:38:00 -0400

    added messaging

--- checkcam.py
+++ checkcam.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import logging
 from logging import handlers
-import os
+import platform
+import subprocess
 import sys
+import simplepush
+
 
 # set logging
 log = logging.getLogger()
@@ -16,13 +19,40 @@ ch.setFormatter(logformat)
 log.addHandler(ch)
 # file handler
 #fh = handlers.RotatingFileHandler("checkiplog.txt", maxBytes=(1048576*5), backupCount=7)
-fh = handlers.RotatingFileHandler("/var/log/wyzecam.log", "a", 5242880, 7)
+fh = handlers.RotatingFileHandler("/var/log/wyzecam.log", maxBytes=(1048576*5), backupCount=7)
+# fh = handlers.RotatingFileHandler("/var/log/wyzecam.log", "a", 5242880, 7)
 fh.setFormatter(logformat)
 log.addHandler(fh)
 
+def ping(host):
+    log.debug("ping function: " + str(host))
+    print("ping function: " + str(host))
+
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    command = ['ping', param, '1', host]
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    
+    log.debug("result: " + str(result))
+    print("result: " + str(result))
+    
+    output = result.stdout.decode('utf8')
+    if "Request timed out." in output or "100% packet loss" in output:
+        msg = ["Camera Offline", "cam " + str(host) + " seems to be offline"]
+        simplepush.ppushitrealgood(msg)
+        return "NOT CONNECTED"
+    
+    msg = ["Camera Online", "yay, cam " + str(host) + " is online"]
+    simplepush.ppushitrealgood(msg)
+    return "CONNECTED"
+
 hostname = '192.168.86.57' #example
-response = os.system("ping " + hostname)
-if response == 0:
-  log.debug(hostname, 'cam is up!')
-else:
-  log.debug(hostname, 'cam is down!')
+# response = os.system("ping " + hostname)
+# if response == 0:
+#   print(hostname, 'cam is up!')
+# else:
+#   print(hostname, 'cam is down!')
+output = ping(hostname)
+print("output = " + str(output))
+log.debug("output = " + str(output))
+
+
