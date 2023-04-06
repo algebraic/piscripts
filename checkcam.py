b83bcfa8d9576faf28e64a9048c75dc7ed279f9b
Zach <zach.johnson47@gmail.com>
2023-04-06 14:27:41 -0400

    ugh, bouncing between machines...

--- checkcam.py
+++ checkcam.py
@@ -4,15 +4,15 @@ from logging import handlers
 
 # set logging
 log = logging.getLogger()
-log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 logformat = logging.Formatter("%(levelname)s %(asctime)s (%(name)s) %(message)s")
 
-fh = handlers.RotatingFileHandler("logs/wyzecam.log", maxBytes=(1048576*5), backupCount=7)
+fh = handlers.RotatingFileHandler("wyzecam.log", maxBytes=(1048576*5), backupCount=7)
 fh.setFormatter(logformat)
 log.addHandler(fh)
 
 ip_address = "192.168.86.57"  # Replace with the IP address you want to ping
-max_attempts = 3          # Maximum number of attempts to ping the IP address
+max_attempts = 4          # Maximum number of attempts to ping the IP address
 
 # Loop through the number of attempts allowed
 for attempt in range(1, max_attempts + 1):
@@ -22,13 +22,13 @@ for attempt in range(1, max_attempts + 1):
 
     # Check the exit code of the ping command
     if ping_process.returncode == 0:
-        log.debug("Successfully pinged {ip_address} on attempt {attempt}.")
+        log.debug(f"Successfully pinged {ip_address} on attempt {attempt}.")
         break  # Stop attempting if ping was successful
     else:
-        log.debug("Ping attempt {attempt} failed. Retrying...")
+        log.debug(f"Ping attempt {attempt} failed. Retrying...")
 
         # Perform action if this is the last attempt
         if attempt == max_attempts:
-            msg = ["Camera Offline", "driveway cam seems to be offline", ""]
-            simplepush.ppushitrealgood(msg, True)
-            log.warning("Failed to ping {ip_address} after {max_attempts} attempts.")
+            # msg = ["Camera Offline", "driveway cam seems to be offline", ""]
+            # simplepush.ppushitrealgood(msg, True)
+            log.warning(f"Failed to ping {ip_address} after {max_attempts} attempts.")
