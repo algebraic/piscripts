b0e6b55d41ff3508c380c1d1de747277944f8986
Zach <zach.johnson47@gmail.com>
2023-04-06 13:32:11 -0400

    fixed logging & message text

--- checkcam.py
+++ checkcam.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
-import logging
+import logging, platform, subprocess, sys, simplepush
 from logging import handlers
-import platform
-import subprocess
-import sys
-import simplepush
-
 
 # set logging
 log = logging.getLogger()
 log.setLevel(logging.INFO)
-#LOG_FILE = "checkiplog.txt"
 logformat = logging.Formatter("%(levelname)s %(asctime)s (%(name)s) %(message)s")
 
 # stdout handler
@@ -18,41 +12,29 @@ ch = logging.StreamHandler(sys.stdout)
 ch.setFormatter(logformat)
 log.addHandler(ch)
 # file handler
-#fh = handlers.RotatingFileHandler("checkiplog.txt", maxBytes=(1048576*5), backupCount=7)
 fh = handlers.RotatingFileHandler("/var/log/wyzecam.log", maxBytes=(1048576*5), backupCount=7)
-# fh = handlers.RotatingFileHandler("/var/log/wyzecam.log", "a", 5242880, 7)
 fh.setFormatter(logformat)
 log.addHandler(fh)
 
 def ping(host):
     log.debug("ping function: " + str(host))
-    print("ping function: " + str(host))
 
     param = '-n' if platform.system().lower()=='windows' else '-c'
     command = ['ping', param, '1', host]
     result = subprocess.run(command, stdout=subprocess.PIPE)
     
     log.debug("result: " + str(result))
-    print("result: " + str(result))
     
     output = result.stdout.decode('utf8')
     if "Request timed out." in output or "100% packet loss" in output:
-        msg = ["Camera Offline", "cam " + str(host) + " seems to be offline", ""]
+        msg = ["Camera Offline", "driveway cam seems to be offline", ""]
         simplepush.ppushitrealgood(msg, True)
-        return "NOT CONNECTED"
+        return "OFFLINE"
     
-    msg = ["Camera Online", "yay, cam " + str(host) + " is online", ""]
-    simplepush.ppushitrealgood(msg, True)
-    return "CONNECTED"
+    # msg = ["Camera Online", "yay, cam " + str(host) + " is online", ""]
+    # simplepush.ppushitrealgood(msg, True)
+    return "online"
 
-hostname = '192.168.86.57' #example
-# response = os.system("ping " + hostname)
-# if response == 0:
-#   print(hostname, 'cam is up!')
-# else:
-#   print(hostname, 'cam is down!')
+hostname = '192.168.86.57'
 output = ping(hostname)
-print("output = " + str(output))
-log.debug("output = " + str(output))
-
-
+log.info("driveway cam " + str(output))
